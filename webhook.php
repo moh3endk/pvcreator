7a46b6c0f5e2c6738a80f31587db23c2758d8457
[M]ohsen <mohsendeluxe@gmail.com>
2021-05-16 00:38:00 +0430

    tech-rashidi.ir

--- webhook.php
+++ webhook.php
@@ -7,7 +7,7 @@ foreach($diff as $value){
     $config = file_get_contents("Bots/".$value."/config.php");
     preg_match_all('/\$Token\s=\s"(.*?)";/', $config, $match);
     $token = $match[1][0];
-    file_get_contents("http://api.telegram.org/bot".$token."/setWebHook?url=Https://tech-rashidi.ir/wp-admin/user/pv/".$value."/index.php");
+    file_get_contents("http://api.telegram.org/bot".$token."/setWebHook?url=Https://ادرس فایل ها".$value."/index.php");
 }
 
 Echo "End";
